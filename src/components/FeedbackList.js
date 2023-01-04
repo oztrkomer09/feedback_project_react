4f202e8024f409a669e06606fc6d64a23f2cf038
omer.ozturk <ofozturk@eskisehir.edu.tr>
2023-01-04 20:23:17 +0300

    added animation finished and 1st phase

--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -1,21 +1,45 @@
+import { motion, AnimatePresence } from "framer-motion";
 import FeedbackItem from "./FeedbackItem";
 
 const FeedbackList = ({ feedbacks, reverse, handleDelete }) => {
   if (!feedbacks || feedbacks.length === 0) {
     return <p>No Feedback Yet</p>;
   }
+
   return (
     <div className="feedback-list">
-      {feedbacks.map((item) => (
-        <FeedbackItem
-          key={item.id}
-          item={item}
-          reverse={reverse}
-          handleDelete={handleDelete}
-        />
-      ))}
+      <AnimatePresence>
+        {feedbacks.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              reverse={reverse}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
+
+  // return (
+  //   <div className="feedback-list">
+  //     {feedbacks.map((item) => (
+  //       <FeedbackItem
+  //         key={item.id}
+  //         item={item}
+  //         reverse={reverse}
+  //         handleDelete={handleDelete}
+  //       />
+  //     ))}
+  //   </div>
+  // );
 };
 
 export default FeedbackList;
